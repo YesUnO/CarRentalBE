c25e3a589460503547d1b89b8256f1c51020a283
František Čech <frantisek.cech@nanoenergies.cz>
2023-03-13 10:31:26 +0100

    context factory fix

--- DataLayer/AppDbContextFactory.cs
+++ DataLayer/AppDbContextFactory.cs
@@ -16,17 +16,6 @@ namespace DataLayer
                 .AddJsonFile("appsettings.json")
                 .Build();
 
-
-
-
-            //var dbContextOptionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
-            //var connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
-            //dbContextOptionsBuilder
-            //    .UseNpgsql(connectionString);
-            //return new ApplicationDbContext(dbContextOptionsBuilder.Options, new OperationalStoreOptions());
-
-
-            //var connectionString = "User ID =postgres;Password=nevermind;Server=localhost;Port=5432;Database=testDb;Integrated Security=true;Pooling=true;";
             var connectionString = configuration.GetConnectionString("DefaultConnection");
             IServiceCollection services = new ServiceCollection();
             services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
