ab04de91b2985d75b4d050ab113760d64e28f037
YesUnO <vilem.cech@seznam.cz>
2023-03-21 11:21:06 +0100

    lil cleanup

--- CarRentalAPI/DependencyInjection.cs
+++ CarRentalAPI/DependencyInjection.cs
@@ -15,14 +15,10 @@ namespace CarRentalAPI
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options =>
                 {
-                    // base-address of your identityserver
                     options.Authority = "https://localhost:7125";
 
-                    // audience is optional, make sure you read the following paragraphs
-                    // to understand your options
                     options.TokenValidationParameters.ValidateAudience = false;
 
-                    // it's recommended to check the type header to avoid "JWT confusion" attacks
                     options.TokenValidationParameters.ValidTypes = new[] { "at+jwt" };
                 });
 
--- DataLayer/DependencyInjection.cs
+++ DataLayer/DependencyInjection.cs
@@ -1,14 +1,8 @@
-﻿using Duende.IdentityServer.AspNetIdentity;
-using Duende.IdentityServer.EntityFramework.Entities;
-using Duende.IdentityServer.Models;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Hosting;
+﻿using Duende.IdentityServer.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using ApiResource = Duende.IdentityServer.Models.ApiResource;
-using ApiScope = Duende.IdentityServer.Models.ApiScope;
 
 namespace DataLayer
 {
@@ -20,9 +14,7 @@ namespace DataLayer
             services.AddDbContext<ApplicationDbContext>(options =>
             options.UseNpgsql(connectionString));
 
-            //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-            //    .AddEntityFrameworkStores<ApplicationDbContext>();
-
+            //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn
             services.AddIdentity<IdentityUser, IdentityRole>()
                 .AddClaimsPrincipalFactory<AdditionalUserClaimsPrincipalFactory>()
                 .AddRoles<IdentityRole>()
@@ -36,10 +28,6 @@ namespace DataLayer
                 new IdentityResources.Profile()
             };
 
-            var resources = configuration.GetSection("IdentityServer:ApiResources").Get<List<ApiResource>>();
-            var scopes = configuration.GetSection("IdentityServer:ApiScopes").Get<List<ApiScope>>();
-            var clients = configuration.GetSection("IdentityServer:Clients").Get<List<Duende.IdentityServer.Models.Client>>();
-
             services.AddIdentityServer(options =>
             {
                 options.EmitStaticAudienceClaim = true;
@@ -48,14 +36,6 @@ namespace DataLayer
                 .AddInMemoryApiScopes(configuration.GetSection("IdentityServer:ApiScopes"))
                 .AddInMemoryApiResources(configuration.GetSection("IdentityServer:ApiResources"))
                 .AddInMemoryClients(configuration.GetSection("IdentityServer:Clients"))
-                //.AddConfigurationStore(options =>
-                //{
-                //    options.ConfigureDbContext = b => b.UseNpgsql(connectionString);
-                //})
-                //.AddOperationalStore(options =>
-                //{
-                //    options.ConfigureDbContext = b => b.UseNpgsql(connectionString);
-                //})
                 .AddInMemoryIdentityResources(identityResources);
                 //.AddResourceOwnerValidator < **PasswordAuthentication * *> ();
             services.AddScoped<IUserClaimsPrincipalFactory<IdentityUser>, AdditionalUserClaimsPrincipalFactory>();
