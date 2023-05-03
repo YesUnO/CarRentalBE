1af73bec122050d9d7b0595d124ec11c619698dc
František Čech <frantisek.cech@nanoenergies.cz>
2023-05-03 22:31:46 +0200

    admin users page WIP

---
+++ Core/ControllerModels/User/UserForAdminModel.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Core.ControllerModels.User
+{
+    public class UserForAdminModel
+    {
+        public string Email { get; set; }
+        public bool HasEmailVerified { get; set; }
+        public string DrivingLicenseImgBack { get; set; }
+        public string DrivingLicenseImgFront { get; set; }
+        public bool HasDrivingLicenseVerified { get; set; }
+        public string IdCardImgBack { get; set; }
+        public string IdCardImgFront { get; set; }
+        public bool HasIdCardVerified { get; set; }
+        public bool HasActivePaymentCard { get; set; }
+        public bool IsApprooved { get; set; }
+    }
+}
